2d21514b051b2faa0a052f5e208e94c449520916
Garrett Breach <breachg1992@gmail.com>
2020-11-02 19:56:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i <array.length; i++) {
-    if (array[i] == skipValue) {
+    if (array[i] == stopValue) {
       break }
       array[i] = changeValue
 }
